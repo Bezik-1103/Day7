31af773f0d0c6e8996e1130afa247523a5d2b875
Bezedinov Miroslav <miroslavbezedinov@gmail.com>
2021-11-01 17:24:18 +0200

    first commit

---
+++ src/components/store.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+Vue.use(Vuex)
+
+const store =new Vuex.Store({
+    plugins:[createPersistedState()],
+    state:{
+        count:0,
+        user:null
+    },
+    mutations: {
+        setCount:(state,c)=> state.count = c,
+        setUser:(state,user)=> state.user = user,
+        deleteUser:(state) => state.user = null,
+    },
+    getters: {
+        getCount:(state,c)=>{
+            return state.count
+        } ,
+        getUser:(state)=>{
+            return state.user
+        } ,
+    }
+})
+export default store;
